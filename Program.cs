f4238147f858dbac8096bca760224acda783067d
tderamus <deramust@gmail.com>
2025-01-18 13:29:28 -0600

    added greeting and option for user input when guessing a number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string greeting = "Welcome to the Number guessing game! \n\nGUESS A NUMBER TO PLAY";
+Console.WriteLine(greeting);
+
+int userGuess = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine($"\nYour guess was {userGuess} !");
