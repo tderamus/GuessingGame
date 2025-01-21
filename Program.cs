a4677f66c5db9b3105fdd1ad15f32370ba23a48a
tderamus <deramust@gmail.com>
2025-01-21 14:10:43 -0600

    added counter option keep up with user attempts

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
-﻿string greeting = "Welcome to the Number guessing game! \n\nGUESS A NUMBER TO PLAY";
+﻿string greeting = "Welcome to the Number guessing game! \n\nGUESS A NUMBER TO PLAY. \nYou have four attempts to guess the number.";
 Console.WriteLine(greeting);
 
 int userGuess = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine($"\nYour guess was {userGuess} !");
+
+int SecretNumber = 42;
+
+
+
+for (int Counter = 0; Counter < 3; Counter++)
+{
+    int userTries = 3 - Counter;
+    if (userGuess == SecretNumber)
+    {
+        Console.WriteLine("Congratulations, your guess is correct!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"So sorry, you guessed the wrong number! \nPlease try again. \nYou have {userTries} tries remaining.");
+        userGuess = Convert.ToInt32(Console.ReadLine());
+    }
+}
