f9b10d98b4a3334f79c1874ffe5da126c42dff4a
tderamus <deramust@gmail.com>
2025-01-21 14:49:19 -0600

    Phase 5 and 6 updates. Use method to test user input hirgher or lower value than planned spending for teh month.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿string greeting = "Welcome To The Number Guessing Game! \n\nGuess a number to play. \nYou have four attempts to guess the number.";
 Console.WriteLine(greeting);
 
-int userGuess = Convert.ToInt32(Console.ReadLine()); 
+int userGuess = Convert.ToInt32(Console.ReadLine());
 
-int SecretNumber = 42;
-
+Random random = new Random();
+int SecretNumber = random.Next(1, 101);
 
 
 for (int Counter = 0; Counter < 3; Counter++)
@@ -12,12 +12,26 @@ for (int Counter = 0; Counter < 3; Counter++)
     int userTries = 3 - Counter;
     if (userGuess == SecretNumber)
     {
-        Console.WriteLine($"Congratulations, your guess {userGuess} is correct!");
+        Console.WriteLine($"Congratulations, your guess is correct!");
+        //UserOutcome(userGuess, SecretNumber);
         break;
     }
     else
     {
-        Console.WriteLine($"So sorry, your guess {userGuess} is incorrect! Please try again. \nYou have {userTries} tries remaining.");
+        Console.WriteLine($"So sorry, your guess is incorrect! Please try again. \nYou have {userTries} tries remaining.");
+        UserOutcome(userGuess, SecretNumber);
         userGuess = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void UserOutcome(int userGuess, int SecretNumber)
+{
+    if (userGuess > SecretNumber)
+    {
+        Console.WriteLine($"Your guess {userGuess} is higher than the secret number!");
+    }
+    else
+    {
+        Console.WriteLine($"Your guess {userGuess} is lower than the secret number!");
     }
 }
