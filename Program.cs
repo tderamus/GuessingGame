5634827aeeb3d072ea1e3d7a6ff0d55120847a88
tderamus <deramust@gmail.com>
2025-01-21 14:17:24 -0600

    display user guess and remaining attempts for user info.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string greeting = "Welcome to the Number guessing game! \n\nGUESS A NUMBER TO PLAY. \nYou have four attempts to guess the number.";
+﻿string greeting = "Welcome To The Number Guessing Game! \n\nGuess a number to play. \nYou have four attempts to guess the number.";
 Console.WriteLine(greeting);
 
 int userGuess = Convert.ToInt32(Console.ReadLine()); 
@@ -12,12 +12,12 @@ for (int Counter = 0; Counter < 3; Counter++)
     int userTries = 3 - Counter;
     if (userGuess == SecretNumber)
     {
-        Console.WriteLine("Congratulations, your guess is correct!");
+        Console.WriteLine($"Congratulations, your guess {userGuess} is correct!");
         break;
     }
     else
     {
-        Console.WriteLine($"So sorry, you guessed the wrong number! \nPlease try again. \nYou have {userTries} tries remaining.");
+        Console.WriteLine($"So sorry, your guess {userGuess} is incorrect! Please try again. \nYou have {userTries} tries remaining.");
         userGuess = Convert.ToInt32(Console.ReadLine());
     }
 }
